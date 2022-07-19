a14aa3be62f4e0d8bf8f22715ee406ba866a8c24
Dilip Chauhan <65770837+TechnicalDC@users.noreply.github.com>
2022-07-19 16:44:37 +0530

    Adding some autocmds

--- lua/autocmds/init.lua
+++ lua/autocmds/init.lua
@@ -1,5 +1,35 @@
 
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
 local autocmd = vim.api.nvim_create_autocmd
 
 -- Vertically center documents when in insert mode
 autocmd({"InsertEnter"},{pattern = {"*"}, command = "norm zz" })
+
+-- Dashboard
+autocmd({"FileType"}, 
+	{
+		pattern = "dashboard", 
+		command = ':lua vim.api.nvim_set_keymap("n", "<leader>ff", ":Telescope find_files<CR>", { noremap = true, silent = true })'
+	}
+)
+autocmd({"FileType"}, 
+	{
+		pattern = "dashboard", 
+		command = ':lua vim.api.nvim_set_keymap("n", "<leader>fh", ":Telescope oldfiles<CR>", { noremap = true, silent = true })'
+	}
+)
+autocmd({"FileType"}, 
+	{
+		pattern = "dashboard", 
+		command = ':lua vim.api.nvim_set_keymap("n", "<leader>cn", ":DashboardNewFile<CR>", { noremap = true, silent = true })'
+	}
+)
+
+-- Telescope
+autocmd({"FileType"}, 
+	{
+		pattern = "TelescopePrompt", 
+		command = 'IndentBlanklineDisable'
+	}
+)
