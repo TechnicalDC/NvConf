3ced032e503e584845be7a78945f2a74fa7e782b
Dilip Chauhan <65770837+TechnicalDC@users.noreply.github.com>
2022-07-20 18:08:31 +0530

    Update dashboard.lua

--- lua/plugins-config/dashboard.lua
+++ lua/plugins-config/dashboard.lua
@@ -39,26 +39,14 @@ db.custom_center = {
 	{
 		icon = '  ',
 		desc = 'Recently opened files                   ',
-		action =  'DashboardFindHistory',
 		shortcut = 'SPC f h'
 	},
 	{
 		icon = '  ',
 		desc = 'Find  File                              ',
-		action = ':Telescope find_files find_command=rg,--hidden,--files',
 		shortcut = 'SPC f f'
 	},
-	{
-		icon = '  ',
-		desc ='File Browser                            ',
-		action =  'Telescope file_browser',
-		shortcut = 'SPC f b'
-	},
-	{
-		icon = '  ',
-		desc = 'Find  word                              ',
-		action = 'Telescope live_grep',
-		shortcut = 'SPC f w'
-	},
 }
+
+
 
