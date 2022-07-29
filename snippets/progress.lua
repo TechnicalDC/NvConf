f02a8fddd44f8827d3ba7f6f2be3f11d3fd7ae39
Dilip Chauhan <65770837+TechnicalDC@users.noreply.github.com>
2022-07-29 12:02:38 +0530

    Update progress.lua

--- snippets/progress.lua
+++ snippets/progress.lua
@@ -13,17 +13,71 @@ local rep = require("luasnip.extras").rep
 
 local snippets = {}
 
-local addDate = fmt(
+local date = function() return {os.date('%d-%m-%Y')} end
+
+local selection = f(function(_, snip)
+			return snip.env.TM_SELECTED_TEXT[1] or {}
+		end, {})
+
+local patch_fmt = fmt(
 	[[
-	Hello {}!
+	/* Last modified on: {} Last modified by: Dilip Chauhan ECO: ALT{} */
 	]],
 	{
-		i(i, "Dilip"),
+		f(function(_,snip) return {os.date('%d-%m-%Y')} end, {}),
+		f(function(_,snip) return {os.date('%d%m%Y')} end, {})
 	}
 )
+local patch_snippet = s("patch", patch_fmt)
 
-local addDate_Snippet = s("hello", addDate)
+local cmmt_fmt = fmt(
+	[[
+	/*
+	{}
+	*/
+	]],
+	{
+		d(1, function(_, snip)
+			return sn(1, t(snip.env.TM_SELECTED_TEXT[1] or {}))
+		end)
+	}
+)
+local cmmt_snippet = s("comment", cmmt_fmt)
 
-table.insert(snippets, addDate_Snippet)
+local def_fmt = fmt(
+	[[
+	define {} {} {}.
+	]],
+	{
+		c(1, {
+			i(1, "variable"), 
+			i(1, "input parameter"),
+			i(1, "output parameter"),
+			i(1, "query"),
+			i(1, "frame"),
+		}),
+		i(2, "<++>"),
+		-- c(2, {
+		-- 	i(1, "<++>"),
+		-- 	d(1, function(_, snip)
+		-- 		return sn(1, t(snip.env.TM_SELECTED_TEXT[1] or {}))
+		-- 	end)
+		-- }),
+		c(3, {
+			i(1, "as character"), 
+			i(1, "as integer"),
+			i(1, "as decimal"),
+			i(1, "as date"),
+			i(1, "as logical"),
+			i(1, "like"),
+			i(1, "for"),
+		}),
+	}
+)
+local def_snippet = s("define", def_fmt)
+
+table.insert(snippets, patch_snippet)
+table.insert(snippets, cmmt_snippet)
+table.insert(snippets, def_snippet)
 
 return snippets
