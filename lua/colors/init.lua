85fc08cf63a4b7ead582cfb0dec3707069d21a7c
Dilip Chauhan <65770837+TechnicalDC@users.noreply.github.com>
2022-08-04 16:45:18 +0530

    Delete lua/colors directory

--- lua/colors/init.lua
+++
@@ -1,8 +0,0 @@
-require("colors.tokyonight")
-require("colors.pywal")
-require("colors.onedark")
-require("colors.dracula")
-require("colors.catppuccin")
-require("colors.gruvbox")
-
-vim.cmd[[colorscheme catppuccin]]
