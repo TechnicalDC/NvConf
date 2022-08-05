db085bff7a25428d4f139c65465e48afd8004155
Dilip Chauhan <chauhandilip305@gmail.com>
2022-08-06 04:26:58 +0530

    Updated on 06 August 2022

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -52,6 +52,7 @@ map("n", "<C-h>", "<C-w>h", opts)
 map("n", "<C-j>", "<C-w>j", opts)
 map("n", "<C-k>", "<C-w>k", opts)
 map("n", "<C-l>", "<C-w>l", opts)
+map("n", "<A-t>", "<C-w>T", opts)
 
 -- Keybindings for split resizing
 map("n", "<A-l>", ":vertical resize +2<CR>", opts)
