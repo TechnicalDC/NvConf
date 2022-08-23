ef1d20be5b219b88dd79163b0c569e7aa8211a92
Dilip Chauhan <65770837+TechnicalDC@users.noreply.github.com>
2022-08-23 14:18:25 +0530

    Update bufferline.lua

--- lua/plugins-config/bufferline.lua
+++ lua/plugins-config/bufferline.lua
@@ -9,7 +9,10 @@ require("bufferline").setup{
 		-- NOTE: this plugin is designed with this icon in mind,
 		-- and so changing this is NOT recommended, this is intended
 		-- as an escape hatch for people who cannot bear it for whatever reason
-		indicator_icon = '',
+		indicator = {
+			icon = '▎', -- this should be omitted if indicator style is not 'icon'
+			style = 'none' -- 'icon' | 'underline' | 'none',
+		},
 		buffer_close_icon = '',
 		modified_icon = '●',
 		close_icon = '',
@@ -64,12 +67,12 @@ require("bufferline").setup{
 		persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
 		-- can also be a table containing 2 custom separators
 		-- [focused and unfocused]. eg: { '|', '|' }
-		separator_style = "slant", -- "slant" | "thick" | "thin" | { 'any', 'any' },
+		separator_style = "thin", -- "slant" | "thick" | "thin" | { 'any', 'any' },
 		enforce_regular_tabs = true,
 		always_show_bufferline = true,
 		sort_by = 'insert_at_end' -- 'insert_after_current' |'insert_at_end' | 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
 			-- add custom logic
-		-- 	return buffer_a.modified > buffer_b.modified
-		-- end
+			-- 	return buffer_a.modified > buffer_b.modified
+			-- end
+		}
 	}
-}
