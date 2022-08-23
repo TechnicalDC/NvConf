ee0151c4eef44beb0292b233b73c4e8a7e94abc0
Dilip Chauhan <65770837+TechnicalDC@users.noreply.github.com>
2022-08-23 16:56:29 +0530

    Update neorg.lua

--- lua/plugins-config/neorg.lua
+++ lua/plugins-config/neorg.lua
@@ -2,11 +2,12 @@ require('neorg').setup {
     load = {
         ["core.defaults"] = {},
 		["core.export"] = {},
+		["core.norg.concealer"] = {},
 		["core.norg.dirman"] = {
-            config = {
-                workspaces = {
-                    work = "~/Documents/work",
-                    home = "~/Documents/home",
+            		config = {
+                	workspaces = {
+                    		work = "~/Documents/work",
+                    		home = "~/Documents/home",
                 }
             }
         }
