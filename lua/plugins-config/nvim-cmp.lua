242dd3650dac8b064d28016f68f54e033595c80d
Dilip Chauhan <65770837+TechnicalDC@users.noreply.github.com>
2022-11-09 10:04:43 +0530

    Update nvim-cmp.lua

--- lua/plugins-config/nvim-cmp.lua
+++ lua/plugins-config/nvim-cmp.lua
@@ -84,6 +84,7 @@ cmp.setup({
 			}
 		},
 		{ name = 'path' },
+		{ name = 'orgmode' },
 	}),
 	experimental = {
 		ghost_text = true
@@ -124,7 +125,11 @@ cmp.setup.cmdline(':', {
 	})
 })
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+-- Following line is deprecated
+-- local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 require('lspconfig')['pyright'].setup {
 	capabilities = capabilities
