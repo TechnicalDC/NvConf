280d32f81609722e3a6100d1d5244499fa373eea
Dilip Chauhan <chauhandilip305@gmail.com>
2022-12-07 22:23:48 +0530

    Updated on 07 December 2022

--- lua/highlights/init.lua
+++ lua/highlights/init.lua
@@ -5,9 +5,15 @@ hi(0, 'TSComment', {link = "Comment"})
 hi(0, 'VertSplit', {link = "Comment"})
 hi(0, 'CmpItemAbbr', {guibg = None})
 
--- BufferLine Highlighting
+-- BufferLine Highlightingb {{{
 hi(0, 'BufferLineFill', {link = "CursorLine"})
 hi(0, 'BufferLineBufferSelected', {link = "lualine_a_normal"})
+hi(0, 'BufferLineIndicatorSelected', {link = "lualine_a_normal"})
+hi(0, 'BufferLineSeparatorSelected', {link = "lualine_a_normal"})
 hi(0, 'BufferLineCloseButtonSelected', {link = "lualine_a_normal"})
 hi(0, 'BufferLineModifiedSelected', {link = "lualine_a_normal"})
 
+hi(0, 'BufferLineTabClose', {link = "ErrorMsg"})
+hi(0, 'BufferLineTabSelected', {link = "lualine_a_normal"})
+hi(0, 'BufferLineTabSeparatorSelected', {link = "lualine_a_normal"})
+-- }}}
--- lua/plugins-config/bufferline.lua
+++ lua/plugins-config/bufferline.lua
@@ -8,7 +8,7 @@ require("bufferline").setup{
 		middle_mouse_command = nil,          -- can be a string | function, see "Mouse actions"
 		indicator = {
 			icon = '▎', -- this should be omitted if indicator style is not 'icon'
-			style = 'none', -- 'icon' | 'underline' | 'none',
+			style = 'icon', -- 'icon' | 'underline' | 'none',
 		},
 		buffer_close_icon = '',
 		modified_icon = '●',
@@ -77,11 +77,6 @@ require("bufferline").setup{
 		separator_style = "thin", -- "slant" | "thick" | "thin" | { 'any', 'any' },
 		enforce_regular_tabs = false, -- false | true,
 		always_show_bufferline = true, -- true | false,
-		hover = {
-			enabled = true,
-			delay = 200,
-			reveal = {'close'}
-		},
 		sort_by = 'insert_at_end' -- 'insert_after_current' |'insert_at_end' | 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
 			-- add custom logic
 		-- 	return buffer_a.modified > buffer_b.modified
