f4a6673f46566fc44be28360d6cfadc9852cdab6
Dilip Chauhan <65770837+TechnicalDC@users.noreply.github.com>
2022-12-16 17:57:30 +0530

    Update init.lua

--- lua/highlights/init.lua
+++ lua/highlights/init.lua
@@ -5,9 +5,11 @@ hi(0, 'TSComment', {link = "Comment"})
 hi(0, 'VertSplit', {link = "Comment"})
 hi(0, 'CmpItemAbbr', {guibg = None})
 
--- BufferLine Highlightingb {{{
+-- BufferLine Highlightings {{{
 hi(0, 'BufferLineFill', {link = "CursorLine"})
 hi(0, 'BufferLineBufferSelected', {link = "lualine_a_normal"})
+hi(0, 'BufferLineBackground', {link = "lualine_c_normal"})
+hi(0, 'BufferLineCloseButton', {link = "lualine_c_normal"})
 hi(0, 'BufferLineIndicatorSelected', {link = "lualine_a_normal"})
 hi(0, 'BufferLineSeparatorSelected', {link = "lualine_a_normal"})
 hi(0, 'BufferLineCloseButtonSelected', {link = "lualine_a_normal"})
