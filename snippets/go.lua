2670fb92c63e076e7494d6e4a53b124650f93ab5
Dilip Chauhan <chauhandilip305@gmail.com>
2022-12-25 21:21:18 +0530

    Updated on 25 December 2022

---
+++ snippets/go.lua
@@ -0,0 +1,40 @@
+-- REQUIRED {{{
+local ls = require("luasnip")
+local s = ls.s --> snippet
+local i = ls.i --> insert node
+local t = ls.t --> text node
+
+local d = ls.dynamic_node
+local c = ls.choice_node
+local f = ls.function_node
+local sn = ls.snippet_node
+
+local fmt = require("luasnip.extras.fmt").fmt
+local rep = require("luasnip.extras").rep
+
+local snippets, autosnippets = {}, {}
+-- }}}
+
+-- BOILER PLATE SNIPPET {{{
+local boiler = fmt(
+	[[
+	package main
+
+	import (
+		"fmt"
+	)
+
+	func main() {{
+		fmt.Println("{}")
+	}}
+	]],
+	{
+		i(1, "<++>"),
+	}
+)
+local boiler_snippet = s("go", boiler)
+table.insert(autosnippets, boiler_snippet)
+-- }}}
+
+return snippets, autosnippets
+
