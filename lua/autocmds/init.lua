42ed604c2481a4efb7f5753fbc0549f488e50919
TechnicalDC <chauhandilip305@gmail.com>
2023-04-08 23:33:59 +0530

    Updated

--- lua/autocmds/init.lua
+++ lua/autocmds/init.lua
@@ -49,21 +49,21 @@ autocmd({"FileType"},
 -- }}}
 
 -- Barbar {{{
-autocmd('BufWinEnter', {
-  pattern = '*',
-  callback = function()
-    if vim.bo.filetype == 'NvimTree' then
-      require'bufferline.api'.set_offset(31, 'FileTree')
-    end
-  end
-})
-
-autocmd('BufWinLeave', {
-  pattern = '*',
-  callback = function()
-    if vim.fn.expand('<afile>'):match('NvimTree') then
-      require'bufferline.api'.set_offset(0)
-    end
-  end
-})
--- }}}
+-- autocmd('BufWinEnter', {
+--   pattern = '*',
+--   callback = function()
+--     if vim.bo.filetype == 'NvimTree' then
+--       require'bufferline.api'.set_offset(31, 'FileTree')
+--     end
+--   end
+-- })
+--
+-- autocmd('BufWinLeave', {
+--   pattern = '*',
+--   callback = function()
+--     if vim.fn.expand('<afile>'):match('NvimTree') then
+--       require'bufferline.api'.set_offset(0)
+--     end
+--   end
+-- })
+-- -- }}}
--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -94,6 +94,17 @@ return {
 			-- usability (see order below)
 			semantic_letters = true,
 
+			sidebar_filetypes = {
+				-- Use the default values: {event = 'BufWinLeave', text = nil}
+				NvimTree = true,
+				-- Or, specify the text used for the offset:
+				undotree = {text = 'undotree'},
+				-- Or, specify the event which the sidebar executes when leaving:
+				['neo-tree'] = {event = 'BufWipeout'},
+				-- Or, specify both
+				Outline = {event = 'BufWinLeave', text = 'symbols-outline'},
+			},
+
 			-- New buffer letters are assigned in this order. This order is
 			-- optimal for the qwerty keyboard layout but might need adjustement
 			-- for other layouts.
