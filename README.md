7706c919287c12b71e160ab9f4bf4b01caf7e1d6
TechnicalDC <chauhandilip305@gmail.com>
2023-04-12 20:52:12 +0530

    updated

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ My own lua based [neovim](https://neovim.org/) configuration.
 </div>
 <br>
 <img src="https://github.com/TechnicalDC/NvConf/blob/main/images/dash.png" alt="img" align="right" width="400px">
+<br>
 
 ## Features
 
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -17,13 +17,13 @@ return {
 
 		local function border(hl_name)
 			return {
-				{ "╭", hl_name },
+				{ "┌", hl_name },
 				{ "─", hl_name },
-				{ "╮", hl_name },
+				{ "┐", hl_name },
 				{ "│", hl_name },
-				{ "╯", hl_name },
+				{ "┘", hl_name },
 				{ "─", hl_name },
-				{ "╰", hl_name },
+				{ "└", hl_name },
 				{ "│", hl_name },
 			}
 		end
