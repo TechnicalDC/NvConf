9d984fc7445d1f3e09996f9c81b551857347be72
Dilip Chauhan <65770837+TechnicalDC@users.noreply.github.com>
2023-06-02 15:47:36 +0530

    Added abl parser

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,6 +1,20 @@
 return {
 	'nvim-treesitter/nvim-treesitter',
 	build = ':TSUpdate',
+	init = function ()
+		local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+		parser_config.abl = {
+			install_info = {
+				url = "https://github.com/usagi-coffee/tree-sitter-abl", -- local path or git repo
+				files = {"src/parser.c"}, -- note that some parsers also require src/scanner.c or src/scanner.cc
+				-- optional entries:
+				branch = "master", -- default branch in case of git repo if different from master
+				generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+				requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+			},
+			filetype = "progress", -- if filetype does not match the parser name
+		}
+	end,
 	config = function ()
 		require'nvim-treesitter.configs'.setup {
 			-- A list of parser names, or "all"
@@ -43,5 +57,6 @@ return {
 				module_path = "nvim-treesitter.incremental_selection"
 			},
 		}
+
 	end
 }
