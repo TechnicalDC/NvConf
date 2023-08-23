5c6c6cfc7de67d4587b0b2b9a71fd498c58ae15b
Dilip Chauhan <65770837+TechnicalDC@users.noreply.github.com>
2023-08-24 00:03:08 +0530

    Update NoiceMini highlight group

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,25 +1,21 @@
----@diagnostic disable: unused-local
+---@diagnostic disable: unused-local, undefined-doc-param
 return {
 	'folke/tokyonight.nvim',
 	lazy = true,
 	config = function ()
 		require("tokyonight").setup({
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
 			style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
 			light_style = "day", -- The theme is used when the background is set to light
-			transparent = false, -- Enable this to disable setting the background color
+			transparent = true, -- Enable this to disable setting the background color
 			terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
 			styles = {
-				-- Style to be applied to different syntax groups
-				-- Value is any valid attr-list value for `:help nvim_set_hl`
-				comments = { italic = false },
+				comments = { italic = true },
 				keywords = { italic = false },
 				functions = {},
 				variables = {},
 				-- Background styles. Can be "dark", "transparent" or "normal"
-				sidebars = "dark", -- style for sidebars, see below
-				floats = "transparent", -- style for floating windows
+				sidebars = "transparent",
+				floats = "transparent",
 			},
 			sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
 			day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
@@ -37,9 +33,11 @@ return {
 			---@param highlights Highlights
 			---@param colors ColorScheme
 			on_highlights = function(hl, colors)
+				hl.NoiceMini = { link = "NoiceVirtualText" }
 				hl.PmenuSel = { link = "lualine_a_insert"}
 				hl.TelescopeSelection = {link = "lualine_a_insert"}
 				hl.BufferTabpages = {link = "lualine_a_visual"}
+				hl.BufferTabpagesSep = {link = "lualine_a_visual"}
 			end,
 		})
 	end
