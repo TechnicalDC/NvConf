25441f6b413ea383f73ac59662a9fd2dfcb49074
Dilip Chauhan <65770837+TechnicalDC@users.noreply.github.com>
2023-11-15 00:31:06 +0530

    Added custom function for `quickfix`

--- lua/options/init.lua
+++ lua/options/init.lua
@@ -1,4 +1,5 @@
 local set = vim.opt
+local fn = vim.fn
 
 set.guicursor		 = ""
 set.swapfile       = false
@@ -36,9 +37,11 @@ set.conceallevel  = 2
 set.concealcursor = 'nc'
 set.termguicolors = true
 set.foldtext	   = "v:lua.custom_fold_text()"
-set.foldcolumn		= "3"
+set.quickfixtextfunc = "v:lua.qftf"
+set.foldcolumn		= "0"
 set.scrolloff	   = 10
 set.undofile		= false
+set.shada			= "!,'300,<50,s10,h"
 
 -- GUI Configuration
 -- set.guifont			= "FantasqueSansMono Nerd Font Mono:style=Regular:pixelsize=10"
@@ -61,6 +64,56 @@ function _G.custom_fold_text()
 	line = string.gsub(line, "{+", "")
 	local line_count = vim.v.foldend - vim.v.foldstart + 1
 	-- local fill_char = vim.o.columns - string.len(line) - string.len(line_count)
-	return line .. ": " .. line_count .. " lines"
-	-- return " " .. line .. ": " .. line_count .. " lines"
+	-- return line .. ": " .. line_count .. " lines"
+	return " " .. line .. ": " .. line_count .. " lines"
+end
+
+function _G.qftf(info)
+    local items
+    local ret = {}
+    -- The name of item in list is based on the directory of quickfix window.
+    -- Change the directory for quickfix window make the name of item shorter.
+    -- It's a good opportunity to change current directory in quickfixtextfunc :)
+    --
+    -- local alterBufnr = fn.bufname('#') -- alternative buffer is the buffer before enter qf window
+    -- local root = getRootByAlterBufnr(alterBufnr)
+    -- vim.cmd(('noa lcd %s'):format(fn.fnameescape(root)))
+    --
+    if info.quickfix == 1 then
+        items = fn.getqflist({id = info.id, items = 0}).items
+    else
+        items = fn.getloclist(info.winid, {id = info.id, items = 0}).items
+    end
+    local limit = 31
+    local fnameFmt1, fnameFmt2 = '%-' .. limit .. 's', '…%.' .. (limit - 1) .. 's'
+    local validFmt = '%s │%5d:%-3d│%s %s'
+    for i = info.start_idx, info.end_idx do
+        local e = items[i]
+        local fname = ''
+        local str
+        if e.valid == 1 then
+            if e.bufnr > 0 then
+                fname = fn.bufname(e.bufnr)
+                if fname == '' then
+                    fname = '[No Name]'
+                else
+                    fname = fname:gsub('^' .. vim.env.HOME, '~')
+                end
+                -- char in fname may occur more than 1 width, ignore this issue in order to keep performance
+                if #fname <= limit then
+                    fname = fnameFmt1:format(fname)
+                else
+                    fname = fnameFmt2:format(fname:sub(1 - limit))
+                end
+            end
+            local lnum = e.lnum > 99999 and -1 or e.lnum
+            local col = e.col > 999 and -1 or e.col
+            local qtype = e.type == '' and '' or ' ' .. e.type:sub(1, 1):upper()
+            str = validFmt:format(fname, lnum, col, qtype, e.text)
+        else
+            str = e.text
+        end
+        table.insert(ret, str)
+    end
+    return ret
 end
