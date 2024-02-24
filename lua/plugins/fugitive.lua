b1e4d22919bae2dc5d08dda14835fb2c7e88abd0
Dilip Chauhan <65770837+TechnicalDC@users.noreply.github.com>
2024-02-24 12:18:26 +0530

    Added keymaps

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,5 +1,19 @@
 return {
 	"tpope/vim-fugitive",
 	enabled = true,
-	cmd = "Git"
+	lazy = false,
+	-- cmd = "Git",
+	config = function ()
+		local map = vim.keymap.set
+		local opts = { noremap = true, silent = true }
+		map("n","<leader>ga", "<cmd>Git add %<cr>", opts)
+		map("n","<leader>gs", "<cmd>Git stage %<cr>", opts)
+		map("n","<leader>gc", function ()
+			vim.ui.input({ prompt = 'Commit message:', default = 'Committed on ' .. os.date('%d-%m-%y') }, function(input)
+				vim.cmd("Git commit -m '" .. input .. "'")
+			end)
+		end, opts)
+		map("n","<leader>gp", "<cmd>Git push<cr>", opts)
+		map("n","<leader>gP", "<cmd>Git pull<cr>", opts)
+	end
 }
