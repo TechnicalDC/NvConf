ad995fc1886256d42bd913c0e4ec2376f574ed00
Dilip Chauhan <65770837+TechnicalDC@users.noreply.github.com>
2024-03-12 23:43:42 +0530

    Delete lua/autocmds directory

--- lua/autocmds/init.lua
+++
@@ -1,9 +0,0 @@
-local autocmd = vim.api.nvim_create_autocmd
-
--- Vertically center documents when in insert mode
-autocmd({"InsertEnter"},{pattern = {"*"}, command = "norm zz" })
-
-autocmd({"BufEnter","BufNewfile"},{pattern = {"*.tex"}, command = "set filetype=tex"})
-autocmd({"BufEnter","BufNewfile"},{pattern = {"*.md"}, command = "set filetype=markdown"})
-autocmd({"BufEnter","BufNewfile"},{pattern = {"*.p","*.i","*.cls"}, command = "set filetype=progress"})
-
