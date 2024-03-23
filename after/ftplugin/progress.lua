0c5f115f84d4370c7ab2aa04cd616239fba4f2ce
Dilip Chauhan <65770837+TechnicalDC@users.noreply.github.com>
2024-03-23 17:58:16 +0530

    Add files via upload

--- after/ftplugin/progress.lua
+++ after/ftplugin/progress.lua
@@ -1,4 +1,6 @@
 ---@diagnostic disable: undefined-global
-local set = vim.opt
+local set = vim.opt_local
 
 set.colorcolumn = "80"
+set.listchars	 = "eol:,tab:  "
+set.conceallevel  = 1
---
+++ after/ftplugin/typescript.lua
@@ -0,0 +1,4 @@
+-- local set = vim.opt
+-- set.tabstop       = 2
+-- set.shiftwidth    = 2
+-- set.softtabstop   = 2
