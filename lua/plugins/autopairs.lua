54c29f3b1af8783a88e512abdbc5d05b616d7458
Dilip Chauhan <65770837+TechnicalDC@users.noreply.github.com>
2024-04-01 23:18:06 +0530

    Delete lua/plugins/autopairs.lua

--- lua/plugins/autopairs.lua
+++
@@ -1,9 +0,0 @@
-return {
-   "windwp/nvim-autopairs",
-   enabled = false,
-   config = function ()
-      require('nvim-autopairs').setup({
-         disable_filetype = { "TelescopePrompt" , "vim" },
-      })
-   end
-}
