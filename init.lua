f25f32f6ba3cbea9af457b98627bfb0895295e7e
Dilip Chauhan <65770837+TechnicalDC@users.noreply.github.com>
2024-04-15 11:00:12 +0530

    latest init.lua

--- init.lua
+++ init.lua
@@ -1,35 +1,63 @@
 ---@diagnostic disable: undefined-global
---      							__           
+--      							__
 -- .-----.-----.-----.--.--.|__|.--------.
 -- |     |  -__|  _  |  |  ||  ||        |
 -- |__|__|_____|_____|\___/ |__||__|__|__|
--- 
+--
 -- Github: https://github.com/TechnicalDC/NvConf
 
 -- REQUIRED
-require("options")
 vim.g.mapleader		= " "
 vim.g.maplocalleader = " "
 
 -- LAZY BOOTSTRAP
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup("plugins")
-
--- REQUIRED
-require("mappings")
-require("autocmds")
-
--- vim.cmd.colorscheme("tokyonight")
-vim.cmd.colorscheme("onedark")
+require('lazy').setup("plugins", {
+	ui = {
+		-- a number <1 is a percentage., >1 is a fixed size
+		size = { width = 0.8, height = 0.8 },
+		wrap = true, -- wrap the lines in the ui
+		-- The border to use for the UI window. Accepts same border values as |nvim_open_win()|.
+		border = "none",
+		title = " Lazy ", ---@type string only works when border is not "none"
+		title_pos = "center", ---@type "center" | "left" | "right"
+		-- Show pills on top of the Lazy window
+		pills = true, ---@type boolean
+		icons = {
+			cmd = " ",
+			config = " ",
+			event = "",
+			ft = " ",
+			init = " ",
+			import = " ",
+			keys = " ",
+			lazy = "󰒲 ",
+			loaded = "●",
+			not_loaded = "○",
+			plugin = " ",
+			runtime = " ",
+			require = "󰢱 ",
+			source = " ",
+			start = "",
+			task = "✔ ",
+			list = {
+				"●",
+				"➜",
+				"★",
+				"‒",
+			},
+		}
+	}
+})
