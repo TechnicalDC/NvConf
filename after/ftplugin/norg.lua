3abf8e521f8ded92395cf96ee361e66d7ebc1d17
Dilip Chauhan <chauhandilip305@gmail.com>
2024-07-04 10:05:47 +0530

    updated trouble

--- after/ftplugin/norg.lua
+++ after/ftplugin/norg.lua
@@ -1,2 +1,12 @@
 ---@diagnostic disable: undefined-global
+local ok, wk = pcall(require, 'which-key')
+
 -- vim.opt.foldcolumn		= "3"
+
+if ok then
+   wk.register({
+      ["<leader>"] = {
+         t = { c = { "<cmd>Neorg toc right<cr>" , "Open Neorg TOC" } }
+      }
+   })
+end
