a1f371aa5ee895bce2fc5f0b7c670288caf9ef00
Dilip Chauhan <chauhandilip305@gmail.com>
2024-07-24 09:47:55 +0530

    updated

--- after/ftplugin/norg.lua
+++ after/ftplugin/norg.lua
@@ -4,9 +4,8 @@ local ok, wk = pcall(require, 'which-key')
 -- vim.opt.foldcolumn		= "3"
 
 if ok then
-   wk.register({
-      ["<leader>"] = {
-         t = { c = { "<cmd>Neorg toc right<cr>" , "Open Neorg TOC" } }
-      }
+   wk.add({
+      mode =  "n",
+      {"<leader>tc", "<cmd>Neorg toc right<cr>" , "Open Neorg TOC" }
    })
 end
