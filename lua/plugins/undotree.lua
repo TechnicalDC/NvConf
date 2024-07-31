e14fa3076582b5487d679f1c59d857f5755bc137
Dilip Chauhan <chauhandilip305@gmail.com>
2024-07-31 16:15:29 +0530

    updated undo tree

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -4,10 +4,11 @@ return {
       vim.keymap.set('n', '<leader>u', "<cmd>UndotreeToggle<cr>", {desc = "Toggle undo tree"})
 
       vim.g.undotree_WindowLayout = 2
+
       vim.g.undotree_TreeNodeShape = ''
       vim.g.undotree_TreeVertShape = '│'
-      vim.g.undotree_TreeSplitShape = '╯'
-      vim.g.undotree_TreeReturnShape = '╮'
+      vim.g.undotree_TreeSplitShape = '─╯'
+      vim.g.undotree_TreeReturnShape = '─╮'
       vim.g.undotree_DiffCommand = "diff"
       vim.g.undotree_RelativeTimestamp = 1
       vim.g.undotree_HighlightChangedText = 1
