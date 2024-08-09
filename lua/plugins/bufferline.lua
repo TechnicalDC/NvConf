8b89024a0c5959ccf1746e900b14987134d420da
Dilip Chauhan <chauhandilip305@gmail.com>
2024-08-09 10:53:52 +0530

    updated

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -13,18 +13,18 @@ return {
             themable = true, -- true | false, -- allows highlight groups to be overriden i.e. sets highlights as default
             numbers = "none", -- "none" | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
             close_command = "bdelete! %d",       -- can be a string | function, | false see "Mouse actions"
-            right_mouse_command = "bdelete! %d", -- can be a string | function | false, see "Mouse actions"
+            right_mouse_command = "bdelete! %d", -- can be a string | function, | false, see "Mouse actions"
             left_mouse_command = "buffer %d",    -- can be a string | function, | false see "Mouse actions"
             middle_mouse_command = nil,          -- can be a string | function, | false see "Mouse actions"
             indicator = {
                icon = '▎', -- this should be omitted if indicator style is not 'icon'
                style = 'none', -- 'icon' | 'underline' | 'none',
             },
-            buffer_close_icon = '󰅖 ',
+            buffer_close_icon = ' ',
             modified_icon = '● ',
             close_icon = ' ',
-            left_trunc_marker = ' ',
-            right_trunc_marker = ' ',
+            left_trunc_marker = '',
+            right_trunc_marker = '',
             --- name_formatter can be used to change the buffer's label in the bufferline.
             --- Please note some names can/will break the
             --- bufferline so use this at your discretion knowing that it has
@@ -103,7 +103,7 @@ return {
                delay = 200,
                reveal = {'close'}
             },
-            sort_by = 'insert_after_current', --'insert_after_current' |'insert_at_end' | 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
+            sort_by = 'insert_at_end', --'insert_after_current' |'insert_at_end' | 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
                -- add custom logic
                --     return buffer_a.modified > buffer_b.modified
                -- end
