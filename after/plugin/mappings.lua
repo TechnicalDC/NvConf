4035a40f87d5ffcfb7317242ca3a31902db0d2ef
Dilip Chauhan <chauhandilip305@gmail.com>
2024-12-12 23:08:47 +0530

    added mini.move and mini.bracketed plugins

--- after/plugin/mappings.lua
+++ after/plugin/mappings.lua
@@ -28,11 +28,7 @@ wk.add({
       { "<leader>cp", "<cmd>cprev<cr>",  desc = "Go to previous item in quickfix list"},
 
       { "<A-=>", "<C-w>=", desc = "Equal the split size" },
-      -- { "<A-S-g>", ":Gitsigns stage_buffer<CR>", desc = "Stage the changes in current buffer" },
-      -- { "<A-t>", "<C-w>T", desc = "Turn split into tab" },
       { "<leader>dt", "<cmd>diffthis<CR>", desc = "Toggle diff for current buffer" },
-      { "<leader>j",  ":m .+1<CR>==",      desc = "Move line down" },
-      { "<leader>k",  ":m .-2<CR>==",      desc = "Move line up" },
       { "<leader>th", "<C-w>t<C-w>K",      desc = "Turn vertical splits to horizontal" },
       { "<leader>tv", "<C-w>t<C-w>H",      desc = "Turn horizontal splits to vertical" },
    },
@@ -40,8 +36,6 @@ wk.add({
       mode = "v",
       { "<", "<gv", desc = "Indent inwards"},
       { ">", ">gv", desc = "Indent outwards"},
-      { "J", ":m '>+1<CR>gv=gv", desc = "Move selected lines down"},
-      { "K", ":m '<-2<CR>gv=gv", desc = "Move selected lines up"},
    }
 })
 
@@ -58,33 +52,33 @@ map("s", "<c-p>", "<cmd>lua require'luasnip'.change_choice(-1)<cr>", opts)
 
 
 -- TMUX {{{
-   -- map("n", "<C-h>", ":lua require('tmux').move_left()<CR>", opts)
-   -- map("n", "<C-j>", ":lua require('tmux').move_bottom()<CR>", opts)
-   -- map("n", "<C-k>", ":lua require('tmux').move_top()<CR>", opts)
-   -- map("n", "<C-l>", ":lua require('tmux').move_right()<CR>", opts)
-   --
-   -- map("n", "<A-l>", ":lua require('tmux').resize_right()<CR>", opts)
-   -- map("n", "<A-h>", ":lua require('tmux').resize_left()<CR>", opts)
-   -- map("n", "<A-k>", ":lua require('tmux').resize_top()<CR>", opts)
-   -- map("n", "<A-j>", ":lua require('tmux').resize_bottom()<CR>", opts)
-   -- -- }}}
+-- map("n", "<C-h>", ":lua require('tmux').move_left()<CR>", opts)
+-- map("n", "<C-j>", ":lua require('tmux').move_bottom()<CR>", opts)
+-- map("n", "<C-k>", ":lua require('tmux').move_top()<CR>", opts)
+-- map("n", "<C-l>", ":lua require('tmux').move_right()<CR>", opts)
+--
+-- map("n", "<A-l>", ":lua require('tmux').resize_right()<CR>", opts)
+-- map("n", "<A-h>", ":lua require('tmux').resize_left()<CR>", opts)
+-- map("n", "<A-k>", ":lua require('tmux').resize_top()<CR>", opts)
+-- map("n", "<A-j>", ":lua require('tmux').resize_bottom()<CR>", opts)
+-- -- }}}
 
-   -- SMART SPLITS {{{{
-      vim.keymap.set('n', '<A-h>', require('smart-splits').resize_left)
-      vim.keymap.set('n', '<A-j>', require('smart-splits').resize_down)
-      vim.keymap.set('n', '<A-k>', require('smart-splits').resize_up)
-      vim.keymap.set('n', '<A-l>', require('smart-splits').resize_right)
-      -- moving between splits
-      vim.keymap.set('n', '<C-h>', require('smart-splits').move_cursor_left)
-      vim.keymap.set('n', '<C-j>', require('smart-splits').move_cursor_down)
-      vim.keymap.set('n', '<C-k>', require('smart-splits').move_cursor_up)
-      vim.keymap.set('n', '<C-l>', require('smart-splits').move_cursor_right)
-      -- swapping buffers between windows
-      -- vim.keymap.set('n', '<leader><leader>h', require('smart-splits').swap_buf_left)
-      -- vim.keymap.set('n', '<leader><leader>j', require('smart-splits').swap_buf_down)
-      -- vim.keymap.set('n', '<leader><leader>k', require('smart-splits').swap_buf_up)
-      -- vim.keymap.set('n', '<leader><leader>l', require('smart-splits').swap_buf_right)
-      -- }}}}
+-- SMART SPLITS {{{{
+vim.keymap.set('n', '<A-h>', require('smart-splits').resize_left)
+vim.keymap.set('n', '<A-j>', require('smart-splits').resize_down)
+vim.keymap.set('n', '<A-k>', require('smart-splits').resize_up)
+vim.keymap.set('n', '<A-l>', require('smart-splits').resize_right)
+-- moving between splits
+vim.keymap.set('n', '<C-h>', require('smart-splits').move_cursor_left)
+vim.keymap.set('n', '<C-j>', require('smart-splits').move_cursor_down)
+vim.keymap.set('n', '<C-k>', require('smart-splits').move_cursor_up)
+vim.keymap.set('n', '<C-l>', require('smart-splits').move_cursor_right)
+-- swapping buffers between windows
+-- vim.keymap.set('n', '<leader><leader>h', require('smart-splits').swap_buf_left)
+-- vim.keymap.set('n', '<leader><leader>j', require('smart-splits').swap_buf_down)
+-- vim.keymap.set('n', '<leader><leader>k', require('smart-splits').swap_buf_up)
+-- vim.keymap.set('n', '<leader><leader>l', require('smart-splits').swap_buf_right)
+-- }}}}
 
 -- Moving text
 map("i", "<A-S-j>", "<Esc>:m .+1<CR>==i", opts)
