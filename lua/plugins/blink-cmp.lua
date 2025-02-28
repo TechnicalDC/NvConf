b7b578461abb8d3cf9ef5ec806df4980eb8ce054
Dilip Chauhan <chauhandilip305@gmail.com>
2025-02-28 11:26:21 +0530

    updated

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -132,7 +132,6 @@ return {
          },
          -- Displays a preview of the selected item on the current line
          ghost_text = {
-            enabled = true,
             enabled = false,
          },
       }
