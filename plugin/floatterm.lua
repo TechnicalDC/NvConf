ddf715c92fa835224b11ddb88ddd9e4233f917b2
Dilip Chauhan <chauhandilip305@gmail.com>
2025-03-05 00:41:30 +0530

    updated borders

--- plugin/floatterm.lua
+++ plugin/floatterm.lua
@@ -30,7 +30,7 @@ local function create_floating_window(opts)
       col = col,
       row = row,
       style = "minimal", -- No borders or extra UI elements
-      border = "rounded",
+      border = "single",
    }
 
    -- Create the floating window
