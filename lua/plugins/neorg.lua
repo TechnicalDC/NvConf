583abdf1e7fd7ede58ede891ef455d85ecef7328
Dilip Chauhan <chauhandilip305@gmail.com>
2025-04-01 11:06:07 +0530

    updated

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -2,7 +2,6 @@ return {
    {
       "nvim-neorg/neorg",
       dependencies = {
-         "benlubas/neorg-interim-ls",
          "nvim-lua/plenary.nvim",
       },
       event = "VimEnter",
@@ -58,9 +57,7 @@ return {
                ["core.tempus"] = {},
                ["core.ui.calendar"] = {},
                -- ["core.integrations.nvim-cmp"] = {},
-               -- ["core.completion"] = {
-                  -- config = { engine = { module_name = "external.lsp-completion" } },
-               -- },
+               -- ["core.completion"] = {},
                ["core.dirman"] = { -- Manages Neorg workspaces
                   config = {
                      workspaces = {
@@ -98,48 +95,8 @@ return {
                      -- end,
                   }
                },
-               -- ["external.interim-ls"] = {
-               --    config = {
-               --       -- default config shown
-               --       completion_provider = {
-               --          -- Enable or disable the completion provider
-               --          enable = true,
-               --
-               --          -- Show file contents as documentation when you complete a file name
-               --          documentation = true,
-               --
-               --          -- Try to complete categories provided by Neorg Query. Requires `benlubas/neorg-query`
-               --          categories = false,
-               --
-               --          -- suggest heading completions from the given file for `{@x|}` where `|` is your cursor
-               --          -- and `x` is an alphanumeric character. `{@name}` expands to `[name]{:$/people:# name}`
-               --          people = {
-               --             enable = false,
-               --
-               --             -- path to the file you're like to use with the `{@x` syntax, relative to the
-               --             -- workspace root, without the `.norg` at the end.
-               --             -- ie. `folder/people` results in searching `$/folder/people.norg` for headings.
-               --             -- Note that this will change with your workspace, so it fails silently if the file
-               --             -- doesn't exist
-               --             path = "people",
-               --          }
-               --       }
-               --    }
-               -- },
-            },
+            }
          }
-         -- vim.api.nvim_create_autocmd("LspAttach", {
-         --    callback = function(args)
-         --       local bufnr = args.buf
-         --       local client = vim.lsp.get_client_by_id(args.data.client_id)
-         --       if not client then return end
-         --
-         --       if client.server_capabilities.completionProvider then
-         --          vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
-         --       end
-         --       -- ... your other lsp mappings
-         --    end
-         -- })
       end,
    }
 }
