11cf21b4a0dfcd7aadab4a90dac62bb438f72838
Dilip Chauhan <chauhandilip305@gmail.com>
2025-04-09 21:07:09 +0530

    updated

--- after/plugin/neovide.lua
+++ after/plugin/neovide.lua
@@ -1,17 +1,13 @@
 if vim.g.neovide then
-   vim.o.guifont = "JetBrainsMono NF:h14"
+   vim.o.guifont = "JetBrainsMono NF:h13"
    vim.opt.linespace = 0
    vim.g.neovide_scale_factor = 1.0
-   vim.g.neovide_padding_top = 0
-   vim.g.neovide_padding_bottom = 0
-   vim.g.neovide_padding_right = 0
-   vim.g.neovide_padding_left = 0
+   vim.g.neovide_padding_top = 20
+   vim.g.neovide_padding_bottom = 20
+   vim.g.neovide_padding_right = 20
+   vim.g.neovide_padding_left = 20
    -- g:neovide_opacity should be 0 if you want to unify transparency of content and title bar.
    vim.g.neovide_opacity = 1.0
    vim.g.transparency = 0.0
-   -- Helper function for transparency formatting
-   local alpha = function()
-      return string.format("%x", math.floor((255 * vim.g.transparency) or 0.8))
-   end
-   vim.g.neovide_background_color = "#0f1117" .. alpha()
+   vim.g.neovide_floating_shadow = false
 end
