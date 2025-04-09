afc2317a15538f31197ff3b1d2ebfc0ff185cdab
Dilip Chauhan <chauhandilip305@gmail.com>
2025-04-09 21:07:09 +0530

    updated

--- after/plugin/neovide.lua
+++ after/plugin/neovide.lua
@@ -10,8 +10,4 @@ if vim.g.neovide then
    vim.g.neovide_opacity = 1.0
    vim.g.transparency = 0.0
    -- Helper function for transparency formatting
-   local alpha = function()
-      return string.format("%x", math.floor((255 * vim.g.transparency) or 0.8))
-   end
-   vim.g.neovide_background_color = "#0f1117" .. alpha()
 end
