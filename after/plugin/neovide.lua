066845f1f3af3a00ccff563c02b813c92a7d6e98
Dilip Chauhan <chauhandilip305@gmail.com>
2025-04-10 10:48:10 +0530

    first commit

--- after/plugin/neovide.lua
+++ after/plugin/neovide.lua
@@ -8,6 +8,7 @@ if vim.g.neovide then
    vim.g.neovide_padding_left = 0
    -- g:neovide_opacity should be 0 if you want to unify transparency of content and title bar.
    vim.g.neovide_opacity = 1.0
+   vim.g.neovide_normal_opacity = 1.0
    vim.g.transparency = 0.0
    -- Helper function for transparency formatting
 end
