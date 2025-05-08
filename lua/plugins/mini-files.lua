3947bb6240180b7ddf9fa68de6db6f2ab5d524a2
Dilip Chauhan <chauhandilip305@gmail.com>
2025-05-08 21:37:52 +0530

    updated mini.files

--- lua/plugins/mini-files.lua
+++ lua/plugins/mini-files.lua
@@ -1,24 +1,59 @@
 return {
    'echasnovski/mini.files',
    version = '*',
+   keys = {
+      {
+         "<leader>of",
+         function()
+            require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+         end,
+         desc = "Open mini.files (Directory of Current File)",
+      },
+      {
+         "<leader>oF",
+         function()
+            require("mini.files").open(vim.uv.cwd(), true)
+         end,
+         desc = "Open mini.files (cwd)",
+      },
+   },
    config = function ()
-      require('mini.files').setup({
-         -- Customization of shown content
-         content = {
-            -- Predicate for which file system entries to show
-            filter = nil,
-            -- What prefix to show to the left of file system entry
-            prefix = nil,
-            -- In which order to show file system entries
-            sort = nil,
-         },
+      local miniFiles = require('mini.files')
+      local autocmd = vim.api.nvim_create_autocmd
 
+      local my_prefix = function(fs_entry)
+         if fs_entry.fs_type == 'directory' then
+            -- NOTE: it is usually a good idea to use icon followed by space
+            return ' ', 'MiniFilesDirectory'
+         end
+         return miniFiles.default_prefix(fs_entry)
+      end
+
+      local show_dotfiles = true
+
+      local filter_show = function(fs_entry) return true end
+
+      local filter_hide = function(fs_entry)
+         return not vim.startswith(fs_entry.name, '.')
+      end
+
+      local toggle_dotfiles = function()
+         show_dotfiles = not show_dotfiles
+         local new_filter = show_dotfiles and filter_show or filter_hide
+         miniFiles.refresh({ content = { filter = new_filter } })
+      end
+
+      local set_mark = function(id, path, desc)
+         miniFiles.set_bookmark(id, path, { desc = desc })
+      end
+
+      miniFiles.setup({
          -- Module mappings created only inside explorer.
          -- Use `''` (empty string) to not create one.
          mappings = {
             close       = 'q',
             go_in       = 'l',
-            go_in_plus  = 'L',
+            go_in_plus  = '<CR>',
             go_out      = 'h',
             go_out_plus = 'H',
             mark_goto   = "'",
@@ -26,44 +61,73 @@ return {
             reset       = '<BS>',
             reveal_cwd  = '@',
             show_help   = 'g?',
-            synchronize = '=',
+            synchronize = 's',
             trim_left   = '<',
             trim_right  = '>',
          },
-
-         -- General options
-         options = {
-            -- Whether to delete permanently or move into module-specific trash
-            permanent_delete = true,
-            -- Whether to use for editing directories
-            use_as_default_explorer = true,
-         },
-
-         -- Customization of explorer windows
+         content = { prefix = my_prefix },
          windows = {
             -- Maximum number of windows to show side by side
-            max_number = math.huge,
+            max_number = 3,
             -- Whether to show preview of file/directory under cursor
             preview = true,
             -- Width of focused window
-            width_focus = 50,
+            width_focus = 40,
             -- Width of non-focused window
-            width_nofocus = 30,
+            width_nofocus = 20,
             -- Width of preview window
             width_preview = 40,
          },
       })
 
+      -- Toggle dotfiles
       vim.api.nvim_create_autocmd('User', {
+         pattern = 'MiniFilesBufferCreate',
+         callback = function(args)
+            local buf_id = args.data.buf_id
+            -- Tweak left-hand side of mapping to your liking
+            vim.keymap.set('n', 'g.', toggle_dotfiles, { buffer = buf_id })
+         end,
+      })
+
+      -- Setting bookmarks
+      vim.api.nvim_create_autocmd('User', {
+         pattern = 'MiniFilesExplorerOpen',
+         callback = function()
+            set_mark('c', vim.fn.stdpath('config'), 'Config') -- path
+            set_mark('w', vim.fn.getcwd, 'Working directory') -- callable
+            set_mark('~', '~', 'Home directory')
+         end,
+      })
+
+      autocmd('User', {
          pattern = 'MiniFilesWindowOpen',
          callback = function(args)
             local win_id = args.data.win_id
 
             -- Customize window-local settings
-            vim.wo[win_id].winblend = 50
             local config = vim.api.nvim_win_get_config(win_id)
-            config.border, config.title_pos = 'rounded', 'left'
+            config.border, config.title_pos = 'rounded', 'center'
             vim.api.nvim_win_set_config(win_id, config)
+         end,
+      })
+      autocmd('User', {
+         pattern = 'MiniFilesWindowUpdate',
+         callback = function(args)
+            local config = vim.api.nvim_win_get_config(args.data.win_id)
+
+            -- Ensure fixed height
+            config.height = 20
+
+            -- Ensure title padding
+            if config.title[#config.title][1] ~= ' ' then
+               table.insert(config.title, { ' ', 'NormalFloat' })
+            end
+            if config.title[1][1] ~= ' ' then
+               table.insert(config.title, 1, { ' ', 'NormalFloat' })
+            end
+
+            vim.api.nvim_win_set_config(args.data.win_id, config)
          end,
       })
    end
