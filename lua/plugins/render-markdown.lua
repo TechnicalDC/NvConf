558f6f41b6ebc0e28cf1f09610ed720df1c5fc8b
Dilip Chauhan <chauhandilip305@gmail.com>
2025-05-15 00:53:54 +0530

    updated

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,6 +1,11 @@
 return {
    'MeanderingProgrammer/render-markdown.nvim',
-   dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+   dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons'
+   },
+   init = function ()
+   end,
    config = function ()
       local render = require('render-markdown')
 
@@ -9,7 +14,24 @@ return {
          anti_conceal = { enabled = false },
          heading = {
             border = false,
-            sign = false
+            sign = false,
+            position = "inline",
+            backgrounds = {
+               'RenderMarkdownH1Bg',
+               'RenderMarkdownH2Bg',
+               'RenderMarkdownH3Bg',
+               'RenderMarkdownH4Bg',
+               'RenderMarkdownH5Bg',
+               'RenderMarkdownH6Bg',
+            },
+            foregrounds = {
+               'RenderMarkdownH1',
+               'RenderMarkdownH2',
+               'RenderMarkdownH3',
+               'RenderMarkdownH4',
+               'RenderMarkdownH5',
+               'RenderMarkdownH6',
+            },
          },
          code = { sign = false },
          quote = { icon = '▎' },
