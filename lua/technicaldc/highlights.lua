70af418bb735508425c8abc4338aca9bc829f711
Dilip Chauhan <chauhandilip305@gmail.com>
2025-05-21 10:25:22 +0530

    updated telescope highlights

--- lua/technicaldc/highlights.lua
+++ lua/technicaldc/highlights.lua
@@ -5,7 +5,7 @@ local palette = require("mini.hues").make_palette()
 
 -- BUILT-IN
 hl(0, "Pmenu", { bg = palette.bg_edge })
--- hl(0, "PmenuSel", { bg = palette.bg_edge })
+hl(0, "PmenuSel", { fg = palette.bg_edge,  bg = palette.fg_edge })
 hl(0, "FloatBorder", { fg = palette.bg_edge, bg = palette.bg_edge })
 hl(0, "FloatTitle", { fg = palette.fg, bg = palette.bg_mid2 })
 
@@ -18,7 +18,7 @@ hl(0, "TelescopeNormal"       , { bg = palette.bg_edge2 })
 hl(0, "TelescopePreviewTitle" , { link = "FloatTitle" })
 hl(0, "TelescopePromptTitle"  , { link = "FloatTitle" })
 hl(0, "TelescopeResultsTitle" , { link = "FloatTitle" })
--- hi(0, "TelescopeSelection"    , { guifg = nil, guibg = darkerstatusline, gui = nil, guisp = nil })
+hl(0, "TelescopeSelection"    , { link = "PmenuSel" })
 -- hi(0, "TelescopePreviewLine"  , { guifg = nil, guibg = M.colors.base01, gui = 'none', guisp = nil })
 
 -- NOICE
