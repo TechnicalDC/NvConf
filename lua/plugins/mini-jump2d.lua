e4e646af826d73aaeeca02a44e055d5532764547
Dilip Chauhan <chauhandilip305@gmail.com>
2025-05-22 21:20:00 +0530

    feat: updated

---
+++ lua/plugins/mini-jump2d.lua
@@ -0,0 +1,7 @@
+return {
+	'echasnovski/mini.jump2d',
+	version = '*',
+	config = function ()
+		require('mini.jump2d').setup()
+	end
+}
--- lua/technicaldc/export.lua
+++ lua/technicaldc/export.lua
@@ -1,36 +1,49 @@
--- Substitute placeholders in the template string with actual values
-local function substitute_template(template_str, values)
-	return template_str:gsub("%${(.-)}", function(key)
-		return values[key] or ""
-	end)
+local util = require("technicaldc.util")
+local palette = require("mini.hues").make_palette()
+local M = {}
+
+function M.pywal()
+	local wal = [[{
+		"wallpaper": "",
+		"special": {
+			"background": "${bg}",
+			"foreground": "${fg}",
+			"cursor": "${fg}",
+		},
+		"colors": {
+			"color0": "${bg}",
+			"color1":  "${red}",
+			"color2":  "${green}",
+			"color3":  "${orange}",
+			"color4":  "${blue}",
+			"color5":  "${purple}",
+			"color6":  "${cyan}",
+			"color7":  "${fg}",
+			"color8":  "${bg_edge2}",
+			"color9":  "${bg_edge}",
+			"color10":  "${bg_mid}",
+			"color11":  "${bg_mid2}",
+			"color12":  "${fg_mid2}",
+			"color13":  "${fg_mid}",
+			"color14":  "${fg_edge}",
+			"color15": "${fg_edge2}"
+		}
+	}
+	]]
+	return util.replace_vars(wal,palette)
 end
 
--- Generate a file using a string template and values
-local function generate_file_from_template(template_str, values, output_path)
-	local result = substitute_template(template_str, values)
+function M.generate(output_path)
+	local result = M.pywal()
 
 	local file = io.open(output_path, "w")
 	if file then
 		file:write(result)
 		file:close()
-		print("File created at: " .. output_path)
+		vim.notify("File created at: " .. output_path,2)
 	else
-		print("Failed to write file.")
+		vim.notify("Failed to write file.",4)
 	end
 end
 
-local template = [[
-{
-	"name": "${name}",
-	"email": "${email}",
-	"role": "${role}"
-}
-]]
-
-local values = {
-	name = "Dilip Chauhan",
-	email = "dilip@example.com",
-	role = "Developer"
-}
-
-generate_file_from_template(template, values, "/home/dilip/output.json")
+return M
