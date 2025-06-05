af6d23b710a3b5207a8dcf01b8e1290222dd4f36
Dilip Chauhan <chauhandilip305@gmail.com>
2025-06-05 11:13:04 +0530

    updated

--- lua/plugins/indentline.lua
+++ lua/plugins/indentline.lua
@@ -1,9 +1,39 @@
+-- Alternatives: ~
+-- • left aligned solid
+-- • `▏`
+-- • `▎` (default)
+-- • `▍`
+-- • `▌`
+-- • `▋`
+-- • `▊`
+-- • `▉`
+-- • `█`
+-- • center aligned solid
+-- • `│`
+-- • `┃`
+-- • right aligned solid
+-- • `▕`
+-- • `▐`
+-- • center aligned dashed
+-- • `╎`
+-- • `╏`
+-- • `┆`
+-- • `┇`
+-- • `┊`
+-- • `┋`
+-- • center aligned double
+-- • `║`
+--
+--
 return {
 	"lukas-reineke/indent-blankline.nvim",
 	main = "ibl",
 	config = function ()
 		require("ibl").setup({
-			indent = { char = ">" },
+			indent = {
+				char = ">",
+				tab_char = ">",
+			},
 			whitespace = {
 				remove_blankline_trail = false,
 			},
