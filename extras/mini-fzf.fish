3a06dd1641942741a4b6e924978adedf0f48aa35
Dilip Chauhan <chauhandilip305@gmail.com>
2025-06-06 10:05:58 +0530

    updated

--- extras/mini-fzf.fish
+++ extras/mini-fzf.fish
@@ -3,7 +3,8 @@ set --export FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS "
 --color=hl:#c6cb9f,hl+:#051a20 \
 --color=prompt:#c0c8cc \
 --color=input-border:#051a20,input-bg:#051a20,query:#c0c8cc \
---color=list-border:#000f15,list-bg:#000f15"
+--color=list-border:#000f15,list-bg:#000f15 \
+--color=preview-border:#000f15,preview-bg:#000f15"
 
 set --export _ZO_FZF_OPTS $_ZO_FZF_OPTS "
 --color=fg:#c0c8cc,fg+:#051a20,bg:-1,bg+:#d2dade \
