2ea9aa5f5563d03abe9b3eb7142fe4b1427be7ee
Dilip Chauhan <chauhandilip305@gmail.com>
2025-06-16 15:47:09 +0530

    initial commit

--- lua/plugins/floatterm.lua
+++ lua/plugins/floatterm.lua
@@ -1,18 +1,20 @@
 return {
     "nvzone/floaterm",
-	 enabled = false,
+	 -- enabled = false,
     dependencies = "nvzone/volt",
-	 opts =  {
-		 border = false,
-		 size = { h = 60, w = 70 },
-
-		 -- to use, make this func(buf)
-		 mappings = { sidebar = nil, term = nil},
-
-		 terminals = {
-			 { name = "Terminal" },
-			 -- { name = "Terminal", cmd = "neofetch" },
-		 },
-	 },
 	 cmd = "FloatermToggle",
+	 config = function ()
+		require("floatterm").setup({
+			 border = false,
+			 size = { h = 80, w = 80 },
+
+			 -- to use, make this func(buf)
+			 mappings = { sidebar = nil, term = nil},
+
+			 terminals = {
+				 { name = "Terminal" },
+				 -- { name = "Terminal", cmd = "neofetch" },
+			 },
+		 })
+	 end
  }
