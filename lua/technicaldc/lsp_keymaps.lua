4efb09f2d7c22a7f91618cb3bc790ca99cc0ede9
Dilip Chauhan <chauhandilip305@gmail.com>
2025-07-20 04:17:40 +0530

    removed neogit

--- lua/technicaldc/lsp_keymaps.lua
+++ lua/technicaldc/lsp_keymaps.lua
@@ -1,7 +1,4 @@
 local map = vim.keymap.set
-local opts = {
-	title = "Hover Title"
-}
 
 map("n", "<leader>rn", vim.lsp.buf.rename,                  { desc = "Rename" } )
 map("n", "<leader>oc", vim.lsp.buf.code_action,             { desc = "Open code actions" } )
@@ -9,7 +6,7 @@ map("n", "<leader>gd", vim.lsp.buf.definition,              { desc = "Go to defi
 map("n", "<leader>gD", vim.lsp.buf.declaration,             { desc = "Go to declaration" } )
 map("n", "<leader>gi", vim.lsp.buf.implementation,          { desc = "Go to implementation" } )
 map("n", "<leader>gr", vim.lsp.buf.references,              { desc = "Go to references" } )
-map("n", "<leader>gt",  vim.lsp.buf.type_definition,         { desc = "Go to type definition" } )
+map("n", "<leader>gt", vim.lsp.buf.type_definition,         { desc = "Go to type definition" } )
 map("n", "<leader>F",  vim.lsp.buf.formatting,              { desc = "Format code" } )
 map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder,    { desc = "Add folder to workspace" } )
 map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, { desc = "Remove folder from workspace" } )
@@ -23,8 +20,7 @@ map( "n", "<leader>wl", function()
 end, { desc = "List workspace folder" })
 
 map( "n", "<leader>K", function ()
-	vim.notify("Custom hover", 3, {})
-	vim.lsp.buf.hover(opts)
+	vim.lsp.buf.hover()
 end, { desc = "Hover docs" })
 
 map( "n", "[d", function()
