7468c82a11b3dc17239f17e126432335c9cb942c
Dilip Chauhan <chauhandilip305@gmail.com>
2025-07-23 11:19:40 +0530

    updated

--- lua/technicaldc/plugins/ui/incline.lua
+++ lua/technicaldc/plugins/ui/incline.lua
@@ -1,5 +1,3 @@
-local helpers = require 'incline.helpers'
-
 local colors = require("mini.hues").make_palette()
 local devicons = require 'nvim-web-devicons'
 
@@ -18,37 +16,10 @@ require('incline').setup {
       local readonly = vim.bo[props.buf].readonly
       local modified_icon = " "
       local readonly_icon = " "
-      local search_icon = ""
 
       if ft_icon == nil then
          ft_icon = ""
       end
-
-      local function get_diagnostic_label()
-         local icons = { Error = "", Warn = "", Hint = "󰌵", Info = ""
-         }
-         local label = {}
-         -- local diagnostics = vim.diagnostic.get(0)
-         -- local count = { 0, 0, 0, 0 }
-         -- for _, diagnostic in ipairs(diagnostics) do
-         -- 	if vim.startswith(vim.diagnostic.get_namespace(diagnostic.namespace).name, 'vim.lsp') then
-         -- 		count[diagnostic.severity] = count[diagnostic.severity] + 1
-         -- 	end
-         -- end
-         -- local error_count = count[vim.diagnostic.severity.ERROR]
-         -- local warning_count = count[vim.diagnostic.severity.WARN]
-         -- local info_count = count[vim.diagnostic.severity.INFO]
-         -- local hint_count = count[vim.diagnostic.severity.HINT]
-
-         for severity, icon in pairs(icons) do
-            local n = #vim.diagnostic.get(props.buf, { severity = vim.diagnostic.severity[string.upper(severity)] })
-            if n > 0 then
-               table.insert(label, { icon .. n .. ' ', group = 'DiagnosticSign' .. severity })
-            end
-         end
-         return label
-      end
-
 
       return {
          -- {get_diagnostic_label()},
