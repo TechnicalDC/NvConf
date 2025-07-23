633ef501b76e5f9ad3394057d58c20790b30a970
Dilip Chauhan <chauhandilip305@gmail.com>
2025-07-23 11:18:56 +0530

    updated

--- lua/technicaldc/plugins/ui/incline.lua
+++ lua/technicaldc/plugins/ui/incline.lua
@@ -1,4 +1,5 @@
 local helpers = require 'incline.helpers'
+
 local colors = require("mini.hues").make_palette()
 local devicons = require 'nvim-web-devicons'
 
@@ -25,47 +26,61 @@ require('incline').setup {
 
       local function get_diagnostic_label()
          local icons = { Error = "", Warn = "", Hint = "󰌵", Info = ""
-      }
-      local label = {}
-      -- local diagnostics = vim.diagnostic.get(0)
-      -- local count = { 0, 0, 0, 0 }
-      -- for _, diagnostic in ipairs(diagnostics) do
-      -- 	if vim.startswith(vim.diagnostic.get_namespace(diagnostic.namespace).name, 'vim.lsp') then
-      -- 		count[diagnostic.severity] = count[diagnostic.severity] + 1
-      -- 	end
-      -- end
-      -- local error_count = count[vim.diagnostic.severity.ERROR]
-      -- local warning_count = count[vim.diagnostic.severity.WARN]
-      -- local info_count = count[vim.diagnostic.severity.INFO]
-      -- local hint_count = count[vim.diagnostic.severity.HINT]
+         }
+         local label = {}
+         -- local diagnostics = vim.diagnostic.get(0)
+         -- local count = { 0, 0, 0, 0 }
+         -- for _, diagnostic in ipairs(diagnostics) do
+         -- 	if vim.startswith(vim.diagnostic.get_namespace(diagnostic.namespace).name, 'vim.lsp') then
+         -- 		count[diagnostic.severity] = count[diagnostic.severity] + 1
+         -- 	end
+         -- end
+         -- local error_count = count[vim.diagnostic.severity.ERROR]
+         -- local warning_count = count[vim.diagnostic.severity.WARN]
+         -- local info_count = count[vim.diagnostic.severity.INFO]
+         -- local hint_count = count[vim.diagnostic.severity.HINT]
 
-      for severity, icon in pairs(icons) do
-         local n = #vim.diagnostic.get(props.buf, { severity = vim.diagnostic.severity[string.upper(severity)] })
-         if n > 0 then
-            table.insert(label, { icon .. n .. ' ', group = 'DiagnosticSign' .. severity })
+         for severity, icon in pairs(icons) do
+            local n = #vim.diagnostic.get(props.buf, { severity = vim.diagnostic.severity[string.upper(severity)] })
+            if n > 0 then
+               table.insert(label, { icon .. n .. ' ', group = 'DiagnosticSign' .. severity })
+            end
          end
+         return label
       end
-      return label
-   end
 
 
-   return {
-      -- {get_diagnostic_label()},
-      ft_icon and {
-         ' ',
-         modified and modified_icon or ft_icon .. ' ',
-         ' ',
-         guibg = modified and colors.red or colors.bg_mid2,
-         guifg = modified and colors.bg or colors.fg
-      } or '',
-      {
-         ' ',
-         filename,
-         ' ',
-         gui = modified and 'italic' or '',
-         guibg = modified and colors.yellow or colors.bg_mid,
-         guifg = modified and colors.bg  or colors.fg
-      },
-   }
-end,
+      return {
+         -- {get_diagnostic_label()},
+         ft_icon and {
+            ' ',
+            ft_icon .. ' ',
+            ' ',
+            guibg = colors.bg_mid2,
+            guifg = colors.fg
+         } or '',
+         {
+            ' ',
+            filename,
+            ' ',
+            gui = modified and 'italic' or '',
+            guibg = colors.bg_mid,
+            guifg = colors.fg
+         },
+         modified and {
+            ' ',
+            modified_icon or ' ',
+            ' ',
+            guibg = colors.yellow,
+            guifg = colors.bg
+         } or '',
+         readonly and {
+            ' ',
+            readonly_icon or ' ',
+            ' ',
+            guibg = colors.red,
+            guifg = colors.bg
+         } or '',
+      }
+   end,
 }
