9d476c215ec7273386b09fa8f04169b6baae3482
Dilip Chauhan <chauhandilip305@gmail.com>
2025-07-24 18:18:11 +0530

    added cursor animation

---
+++ lua/technicaldc/plugins/smear-cursor.lua
@@ -0,0 +1,23 @@
+return {
+   "sphamba/smear-cursor.nvim",
+
+   opts = {
+      -- Smear cursor when switching buffers or windows.
+      smear_between_buffers = true,
+
+      -- Smear cursor when moving within line or to neighbor lines.
+      -- Use `min_horizontal_distance_smear` and `min_vertical_distance_smear` for finer control
+      smear_between_neighbor_lines = true,
+
+      -- Draw the smear in buffer space instead of screen space when scrolling
+      scroll_buffer_space = true,
+
+      -- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
+      -- Smears will blend better on all backgrounds.
+      legacy_computing_symbols_support = false,
+
+      -- Smear cursor in insert mode.
+      -- See also `vertical_bar_cursor_insert_mode` and `distance_stop_animating_vertical_bar`.
+      smear_insert_mode = true,
+   },
+}
