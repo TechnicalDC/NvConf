013876e54d849ac592e755655a99c8f090291478
Dilip Chauhan <chauhandilip305@gmail.com>
2025-08-02 15:57:19 +0530

    updated

--- lua/technicaldc/plugins/ui/noice-ui.lua
+++ lua/technicaldc/plugins/ui/noice-ui.lua
@@ -9,7 +9,7 @@ require("noice").setup({
          -- opts: any options passed to the view
          -- icon_hl_group: optional hl_group for the icon
          -- title: set to anything or empty string to hide
-         cmdline = { pattern = "^:", icon = "󰘳 ", lang = "vim", title = " Command " },
+         cmdline = { pattern = "^:", icon = "", lang = "vim", title = " Command " },
          search_down = { kind = "search", pattern = "^/", icon = "  ", lang = "regex", title = " Search Down " },
          search_up = { kind = "search", pattern = "^%?", icon = "  ", lang = "regex" , title = " Search Up " },
          filter = { pattern = "^:%s*!", icon = "  ", lang = "bash" , title = " Filter " },
