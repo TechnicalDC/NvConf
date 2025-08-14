9377a4dc6b5c7eb1843e9440923d3eecae58ab2d
Dilip Chauhan <chauhandilip305@gmail.com>
2025-08-14 11:10:11 +0530

    replaced oil with mini.files

--- lua/technicaldc/plugins/mini.lua
+++ lua/technicaldc/plugins/mini.lua
@@ -5,9 +5,12 @@ return {
 
       -- IMPORTS {{{
 		local starter = require('mini.starter')
+      local miniFiles = require('mini.files')
 		local autocmd = vim.api.nvim_create_autocmd
 		local map     = vim.keymap.set
 		local headers = require("technicaldc.header_ascii")
+      local show_dotfiles = true
+      local filter_show = function(fs_entry) return true end
       -- }}}
 
       -- OPTS {{{
@@ -41,6 +44,27 @@ return {
             col = vim.o.columns,
             row = vim.o.lines - pad
          }
+      end
+
+      local my_prefix = function(fs_entry)
+         if fs_entry.fs_type == 'directory' then
+            return ' ', 'MiniFilesDirectory'
+         end
+         return miniFiles.default_prefix(fs_entry)
+      end
+
+      local filter_hide = function(fs_entry)
+         return not vim.startswith(fs_entry.name, '.')
+      end
+
+      local toggle_dotfiles = function()
+         show_dotfiles = not show_dotfiles
+         local new_filter = show_dotfiles and filter_show or filter_hide
+         miniFiles.refresh({ content = { filter = new_filter } })
+      end
+
+      local set_mark = function(id, path, desc)
+         miniFiles.set_bookmark(id, path, { desc = desc })
       end
       -- }}}
 
@@ -319,6 +343,39 @@ return {
 			silent = false,
 		})
 
+      miniFiles.setup({
+         -- Module mappings created only inside explorer.
+         -- Use `''` (empty string) to not create one.
+         mappings = {
+            close       = 'q',
+            go_in       = 'l',
+            go_in_plus  = '<CR>',
+            go_out      = 'h',
+            go_out_plus = 'H',
+            mark_goto   = "'",
+            mark_set    = 'm',
+            reset       = '<BS>',
+            reveal_cwd  = '@',
+            show_help   = 'g?',
+            synchronize = 's',
+            trim_left   = '<',
+            trim_right  = '>',
+         },
+         content = { prefix = my_prefix },
+         windows = {
+            -- Maximum number of windows to show side by side
+            max_number = 3,
+            -- Whether to show preview of file/directory under cursor
+            preview = true,
+            -- Width of focused window
+            width_focus = 30,
+            -- Width of non-focused window
+            width_nofocus = 30,
+            -- Width of preview window
+            width_preview = 50,
+         },
+      })
+
 		autocmd("User",{
 			pattern = "MiniStarterOpened",
 			callback = function(args)
@@ -326,6 +383,50 @@ return {
 				vim.opt_local.statuscolumn = ""
 				map("n", "j", "<Cmd>lua MiniStarter.update_current_item('next')<CR>", opts)
 				map("n", "k", "<Cmd>lua MiniStarter.update_current_item('prev')<CR>", opts)
-			end})
+			end
+      })
+
+      autocmd('User', {
+         pattern = 'MiniFilesBufferCreate',
+         callback = function(args)
+            local buf_id = args.data.buf_id
+            -- Tweak left-hand side of mapping to your liking
+            vim.keymap.set('n', 'g.', toggle_dotfiles, { buffer = buf_id })
+         end 
+      })
+
+      autocmd('User', {
+         pattern = 'MiniFilesWindowOpen',
+         callback = function(args)
+            local win_id = args.data.win_id
+            -- Customize window-local settings
+            local config = vim.api.nvim_win_get_config(win_id)
+            config.border, config.title_pos = 'double', 'center'
+            vim.api.nvim_win_set_config(win_id, config)
+         end,
+      })
+
+      autocmd('User', {
+         pattern = 'MiniFilesWindowUpdate',
+         callback = function(args)
+            local config = vim.api.nvim_win_get_config(args.data.win_id)
+
+            vim.wo[args.data.win_id].number = true
+            vim.wo[args.data.win_id].relativenumber = true
+            vim.wo[args.data.win_id].statuscolumn = "%s%=%{v:relnum ? v:relnum : v:lnum} "
+
+
+            -- Ensure title padding
+            if config.title[#config.title][1] ~= ' ' then
+               table.insert(config.title, { ' ', 'NormalFloat' })
+            end
+            if config.title[1][1] ~= ' ' then
+               table.insert(config.title, 1, { ' ', 'NormalFloat' })
+            end
+
+            vim.api.nvim_win_set_config(args.data.win_id, config)
+         end,
+      })
+
    end
 }
