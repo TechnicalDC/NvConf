5b98f3bae76ec39569c06f8ea0a93cffebd5978e
Dilip Chauhan <chauhandilip305@gmail.com>
2025-10-03 02:18:54 +0530

    deleted extras

--- extras/mini-fzf.fish
+++
@@ -1,8 +0,0 @@
-   set --export FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS "
-   --color=fg:#c0c8cc,fg+:#051a20,bg:-1,bg+:#d2dade \
-   --color=border:#d2dade"
-   # 
-   set --export _ZO_FZF_OPTS $_ZO_FZF_OPTS "
-   --color=fg:#c0c8cc,fg+:#051a20,bg:-1,bg+:#d2dade 
-   --color=border:#d2dade"
-	
