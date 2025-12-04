aa6fc4c014d24dd49b1424cf2ed5ef7191c8925d
Dilip Chauhan <chauhandilip305@gmail.com>
2025-12-05 00:37:15 +0530

    updated

--- lua/technicaldc/plugins/noice.lua
+++ lua/technicaldc/plugins/noice.lua
@@ -14,7 +14,7 @@ return {
            search_up = { kind = "search", pattern = "^%?", icon = "   ", lang = "regex" },
            filter = { pattern = "^:%s*!", icon = " $ ", lang = "bash" },
            lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "  ", lang = "lua" },
-           help = { pattern = "^:%s*he?l?p?%s+", icon = "  " },
+           help = { pattern = "^:%s*he?l?p?%s+", icon = " 󰋖 " },
            input = { view = "cmdline_input", icon = " 󰥻  " }, -- Used by input()
            -- lua = false, -- to disable a format, set to `false`
         },
