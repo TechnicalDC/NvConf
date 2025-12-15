2eb3944078152a1900156a60b9565fec4cd3ea0a
Dilip Chauhan <chauhandilip305@gmail.com>
2025-12-15 20:22:03 +0530

    updated

--- lua/technicaldc/plugins/whichkey.lua
+++ lua/technicaldc/plugins/whichkey.lua
@@ -9,7 +9,7 @@ return {
 
       local conf = {
          ---@type false | "classic" | "modern" | "helix"
-         preset = "modern",
+         preset = "classic",
          -- Delay before showing the popup. Can be a number or a function that returns a number.
          ---@type number | fun(ctx: { keys: string, mode: string, plugin?: string }):number
          delay = function(ctx)
